6ba37bb5bd1aab8689070f8570c14a8f375b4d4f
dxdns <37380674+dxdns@users.noreply.github.com>
2024-10-09 09:24:13 -0300

    data chart home page

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from "@angular/common/http"
 import { Component, OnInit } from "@angular/core"
 
 @Component({
@@ -6,7 +7,73 @@ import { Component, OnInit } from "@angular/core"
     styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
-    constructor() {}
+    private apiUrl = "http://localhost:8081/vaccinations"
+    vaccinationData: VaccinationData[] = []
+    filteredData: VaccinationData[] = []
+    countries: string[] = []
+    selectedCountries: string[] = []
 
-    ngOnInit(): void {}
+    constructor(private http: HttpClient) {}
+
+    ngOnInit() {
+        this.fetchData()
+    }
+
+    fetchData(page: number = 0, size: number = 999) {
+        this.http
+            .get<any>(`${this.apiUrl}?page=${page}&size=${size}`)
+            .subscribe(
+                (data) => {
+                    const obj = data.content as VaccinationData[]
+                    this.vaccinationData = obj
+                    this.filteredData = obj
+                    this.countries = Array.from(
+                        new Set(obj.map((item) => item.country))
+                    )
+                },
+                (error) => {
+                    console.error("Erro ao buscar dados:", error)
+                }
+            )
+    }
+
+    toggleCountry(country: string) {
+        const index = this.selectedCountries.indexOf(country)
+        if (index === -1) {
+            this.selectedCountries.push(country)
+        } else {
+            this.selectedCountries.splice(index, 1)
+        }
+
+        this.filterData()
+    }
+
+    filterData() {
+        if (this.selectedCountries.length > 0) {
+            this.filteredData = this.vaccinationData.filter((item) =>
+                this.selectedCountries.includes(item.country)
+            )
+        } else {
+            this.filteredData = this.vaccinationData
+        }
+    }
+}
+
+export interface VaccinationData {
+    id: number
+    country: string
+    isoCode: string
+    date: string
+    totalVaccinations: number
+    peopleVaccinated: number
+    peopleFullyVaccinated: number
+    dailyVaccinationsRaw: number
+    dailyVaccinations: number
+    totalVaccinationsPerHundred: number
+    peopleVaccinatedPerHundred: number
+    peopleFullyVaccinatedPerHundred: number
+    dailyVaccinationsPerMillion: number
+    vaccines: string
+    sourceName: string
+    sourceWebsite: string
 }
--- frontend/src/app/shared/components/chart/chart.component.ts
+++ frontend/src/app/shared/components/chart/chart.component.ts
@@ -1,50 +1,28 @@
-import { Component, OnInit } from "@angular/core"
+import { Component, Input, OnChanges, SimpleChanges } from "@angular/core"
 import * as Plotly from "plotly.js-dist-min"
-import { HttpClient } from "@angular/common/http"
+import { VaccinationData } from "src/app/pages/home/home.component"
 
 @Component({
     selector: "app-chart",
     templateUrl: "./chart.component.html",
     styleUrls: ["./chart.component.css"],
 })
-export class ChartComponent implements OnInit {
-    private apiUrl = "http://localhost:8081/vaccinations"
-    countries: string[] = []
-    selectedCountries: string[] = []
-    private vaccinationData: VaccinationData[] = []
-    private filteredData: VaccinationData[] = []
+export class ChartComponent implements OnChanges {
+    @Input() selectedCountries: string[] = []
+    @Input() filteredData: VaccinationData[] = []
 
-    constructor(private http: HttpClient) {}
-
-    ngOnInit() {
-        this.fetchData()
+    ngOnChanges(changes: SimpleChanges): void {
+        if (changes.filteredData && this.filteredData.length > 0) {
+            this.createChart(this.filteredData)
+        }
     }
 
-    fetchData(page: number = 0, size: number = 99999) {
-        this.http
-            .get<any>(`${this.apiUrl}?page=${page}&size=${size}`)
-            .subscribe(
-                (data) => {
-                    const obj = data.content as VaccinationData[]
-                    this.vaccinationData = obj
-                    this.filteredData = obj
-                    this.createChart()
-                    this.countries = Array.from(
-                        new Set(obj.map((item) => item.country))
-                    )
-                },
-                (error) => {
-                    console.error("Erro ao buscar dados:", error)
-                }
-            )
-    }
-
-    createChart() {
-        const dates = this.filteredData.map((item) => item.date)
-        const totalVaccinations = this.filteredData.map(
+    createChart(filteredData: VaccinationData[]) {
+        const dates = filteredData.map((item) => item.date)
+        const totalVaccinations = filteredData.map(
             (item) => item.totalVaccinations
         )
-        const dailyVaccinationsPerMillion = this.filteredData.map(
+        const dailyVaccinationsPerMillion = filteredData.map(
             (item) => item.dailyVaccinationsPerMillion
         )
 
@@ -92,45 +70,4 @@ export class ChartComponent implements OnInit {
 
         Plotly.newPlot("myDiv", data as any, layout)
     }
-
-    toggleCountry(country: string) {
-        const index = this.selectedCountries.indexOf(country)
-        if (index === -1) {
-            this.selectedCountries.push(country)
-        } else {
-            this.selectedCountries.splice(index, 1)
-        }
-
-        this.filterData()
-    }
-
-    filterData() {
-        if (this.selectedCountries.length > 0) {
-            this.filteredData = this.vaccinationData.filter((item) =>
-                this.selectedCountries.includes(item.country)
-            )
-        } else {
-            this.filteredData = this.vaccinationData
-        }
-        this.createChart()
-    }
-}
-
-export interface VaccinationData {
-    id: number
-    country: string
-    isoCode: string
-    date: string
-    totalVaccinations: number
-    peopleVaccinated: number
-    peopleFullyVaccinated: number
-    dailyVaccinationsRaw: number
-    dailyVaccinations: number
-    totalVaccinationsPerHundred: number
-    peopleVaccinatedPerHundred: number
-    peopleFullyVaccinatedPerHundred: number
-    dailyVaccinationsPerMillion: number
-    vaccines: string
-    sourceName: string
-    sourceWebsite: string
 }
