5b0c6b4472a38b3d8b1fd76256a88e9f2e244b3a
dxdns <37380674+dxdns@users.noreply.github.com>
2024-10-09 09:38:05 -0300

    vaccination model getters & setters

--- src/main/java/com/kaggle/app/models/VaccinationModel.java
+++ src/main/java/com/kaggle/app/models/VaccinationModel.java
@@ -9,8 +9,13 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
 @Table(name = "country_vaccinations")
+@Getter
+@Setter
 public class VaccinationModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -54,127 +59,6 @@ public class VaccinationModel {
         this.dailyVaccinationsPerMillion = dailyVaccinationsPerMillion;
         this.vaccines = vaccines;
         this.sourceName = sourceName;
-        this.sourceWebsite = sourceWebsite;
-    }
-
-    // Getters e Setters
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getIsoCode() {
-        return isoCode;
-    }
-
-    public void setIsoCode(String isoCode) {
-        this.isoCode = isoCode;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public Long getTotalVaccinations() {
-        return totalVaccinations;
-    }
-
-    public void setTotalVaccinations(Long totalVaccinations) {
-        this.totalVaccinations = totalVaccinations;
-    }
-
-    public Long getPeopleVaccinated() {
-        return peopleVaccinated;
-    }
-
-    public void setPeopleVaccinated(Long peopleVaccinated) {
-        this.peopleVaccinated = peopleVaccinated;
-    }
-
-    public Long getPeopleFullyVaccinated() {
-        return peopleFullyVaccinated;
-    }
-
-    public void setPeopleFullyVaccinated(Long peopleFullyVaccinated) {
-        this.peopleFullyVaccinated = peopleFullyVaccinated;
-    }
-
-    public Long getDailyVaccinationsRaw() {
-        return dailyVaccinationsRaw;
-    }
-
-    public void setDailyVaccinationsRaw(Long dailyVaccinationsRaw) {
-        this.dailyVaccinationsRaw = dailyVaccinationsRaw;
-    }
-
-    public Long getDailyVaccinations() {
-        return dailyVaccinations;
-    }
-
-    public void setDailyVaccinations(Long dailyVaccinations) {
-        this.dailyVaccinations = dailyVaccinations;
-    }
-
-    public BigDecimal getTotalVaccinationsPerHundred() {
-        return totalVaccinationsPerHundred;
-    }
-
-    public void setTotalVaccinationsPerHundred(BigDecimal totalVaccinationsPerHundred) {
-        this.totalVaccinationsPerHundred = totalVaccinationsPerHundred;
-    }
-
-    public BigDecimal getPeopleVaccinatedPerHundred() {
-        return peopleVaccinatedPerHundred;
-    }
-
-    public void setPeopleVaccinatedPerHundred(BigDecimal peopleVaccinatedPerHundred) {
-        this.peopleVaccinatedPerHundred = peopleVaccinatedPerHundred;
-    }
-
-    public BigDecimal getPeopleFullyVaccinatedPerHundred() {
-        return peopleFullyVaccinatedPerHundred;
-    }
-
-    public void setPeopleFullyVaccinatedPerHundred(BigDecimal peopleFullyVaccinatedPerHundred) {
-        this.peopleFullyVaccinatedPerHundred = peopleFullyVaccinatedPerHundred;
-    }
-
-    public BigDecimal getDailyVaccinationsPerMillion() {
-        return dailyVaccinationsPerMillion;
-    }
-
-    public void setDailyVaccinationsPerMillion(BigDecimal dailyVaccinationsPerMillion) {
-        this.dailyVaccinationsPerMillion = dailyVaccinationsPerMillion;
-    }
-
-    public String getVaccines() {
-        return vaccines;
-    }
-
-    public void setVaccines(String vaccines) {
-        this.vaccines = vaccines;
-    }
-
-    public String getSourceName() {
-        return sourceName;
-    }
-
-    public void setSourceName(String sourceName) {
-        this.sourceName = sourceName;
-    }
-
-    public String getSourceWebsite() {
-        return sourceWebsite;
-    }
-
-    public void setSourceWebsite(String sourceWebsite) {
         this.sourceWebsite = sourceWebsite;
     }
 }
